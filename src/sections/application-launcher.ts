410ccfb018024b1ae869e866e454486497406b31
Gabe Dunn <gabe@sent.at>
2024-06-22 17:25:44 -0600

    feat: make app launcher command configurable

--- src/sections/application-launcher.ts
+++ src/sections/application-launcher.ts
@@ -1,11 +1,13 @@
 import { Section, Icon, TransparentButton } from 'src/components'
 
+import config from 'src/config'
+
 export default function ApplicationLauncher() {
   return Section(
     [
       TransparentButton({
         child: Icon('nix-snowflake-white', 18),
-        onPrimaryClick: () => Utils.exec('tofi-drun --drun-launch=true'),
+        onPrimaryClick: () => Utils.exec(config.scripts.appLauncher),
       }),
     ],
     { margin: 0 },
