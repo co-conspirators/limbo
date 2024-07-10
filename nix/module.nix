9c13edaef4f5af9ff5d8b4d9b5b89db0930b8ee5
Gabe Dunn <gabe@sent.at>
2024-07-09 23:17:18 -0600

    fix: update nixos module to use proper package

--- nix/module.nix
+++ nix/module.nix
@@ -4,21 +4,19 @@ let cfg = config.services.limbo;
 in {
   options.services.limbo = (import ./options.nix { inherit lib pkgs; });
 
-  config =
-    let finalPackage = cfg.package.override { defaultConfig = cfg.settings; };
-    in lib.mkIf cfg.enable {
-      environment.systemPackages = [ cfg.package ];
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = [ cfg.package ];
 
-      systemd.user.services.limbo = {
-        description = "limbo service";
-        path = [ cfg.package ];
-        after = [ "graphical-session-pre.target" ];
-        partOf = [ "graphical-session.target" ];
-        wantedBy = [ "graphical-session.target" ];
-        serviceConfig = {
-          ExecStart = "${finalPackage}/bin/limbo";
-          Restart = "always";
-        };
+    systemd.user.services.limbo = {
+      description = "limbo service";
+      path = [ cfg.package ];
+      after = [ "graphical-session-pre.target" ];
+      partOf = [ "graphical-session.target" ];
+      wantedBy = [ "graphical-session.target" ];
+      serviceConfig = {
+        ExecStart = "${cfg.package}/bin/limbo";
+        Restart = "always";
       };
     };
+  };
 }
